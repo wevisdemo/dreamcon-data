ff52e508568a16a7ec83a4bad4890efb33dfce82
patiphan2000 <srichai.patiphan@gmail.com>
2026-09-17 14:15:46 +0700

    feat: setup output

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import os
 import pandas as pd
 from pathlib import Path
 from agglo_cluster import cluster_topic
 from question_generator import generate_question
 
 data_dir_path = Path(__file__).resolve().parent / "data"
+output_dir_path = Path(__file__).resolve().parent / "output"
 
 def main() -> None:
     print("Hello DreamCon")
+    
+    # Check & Create output directory
+    os.makedirs(data_dir_path, exist_ok=True)
     
     # Load topic data
     topic_df = pd.read_csv(data_dir_path / "topics.csv")
@@ -16,8 +21,8 @@ def main() -> None:
     full_questions_df = generate_question(clustered_topic_df)
     
     # Save to csv
-    clustered_topic_df.to_csv("output/topic_groups.csv", index=False)
-    full_questions_df.to_csv("output/group_questions.csv", index=False)
+    clustered_topic_df.to_csv(output_dir_path / "topic_groups.csv", index=False)
+    full_questions_df.to_csv(output_dir_path / "group_questions.csv", index=False)
     
 if __name__ == "__main__":
     main()
